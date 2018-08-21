1fcd5936c48069627e471df480d30d6e0190f150
space-rider <sailorbeavis@gmail.com>
2018-08-21 21:39:04 +0300

    дополнения с правками

---
+++ lesson1/js/lesson1.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+window.onload = function () {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].style.textDecoration = ('none');
+        links[i].style.color = ('black');
+    }
+}
